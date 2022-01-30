f98a5d7153e175c1f9a29df5ec6d4b4660c5b94e
adam0311 <abal.alan7@gmail.com>
2022-01-29 23:56:29 -0300

    HW JS VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -61,7 +61,7 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var 1
+  
 }
 
 // No modificar nada debajo de esta línea
